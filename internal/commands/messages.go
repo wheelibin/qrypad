48aa1887e8d05f4e2ca57f1571549a4408258981
wheelibin <wheelibin@gmail.com>
2024-07-02 22:14:34 +0100

    chore: comments

--- internal/commands/messages.go
+++ internal/commands/messages.go
@@ -1,11 +1,14 @@
 package commands
 
+// all command errors are passed back using this
 type ErrMsg struct{ Err error }
 
 func (e ErrMsg) Error() string { return e.Err.Error() }
 
+// sent when the user navigates to another panel
 type ActivePanelChangedMsg int
 
+// sent when loading has started
 type LoadingMsg struct{}
 
 // contains the selected table name
@@ -14,4 +17,5 @@ type TableSelectedMsg string
 // the contents of the query file
 type QueryFileReadMsg string
 
+// sent when the query file has been saved
 type QueryFileSavedMsg struct{}
