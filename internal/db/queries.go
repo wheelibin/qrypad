02d06f44da55599218ca16aa50e17e9c0473d1ec
wheelibin <wheelibin@gmail.com>
2024-07-04 22:21:23 +0100

    feat: add sql parser to enable proper handling of SELECT vs INSERT/UPDATE etc

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/spf13/viper"
+	"github.com/xwb1989/sqlparser"
 )
 
 type Table struct {
@@ -15,8 +16,8 @@ type Table struct {
 	RowCount int
 }
 
+// fetches the user tables in the database
 func GetSchemaTables(dbConn DBConn) (*Data, error) {
-
 	var query string
 	switch dbConn.DriverName {
 	case DriverNameMySQL:
@@ -32,82 +33,37 @@ func GetSchemaTables(dbConn DBConn) (*Data, error) {
 	}
 
 	return ExecuteQuery(dbConn, query)
-
 }
 
+// fetches the column information for the specified table
 func GetTableColumns(dbConn DBConn, tableName string) (*Data, error) {
 	return ExecuteQuery(dbConn, fmt.Sprintf(`SELECT column_name name, data_type type, case when is_nullable = 'NO' then 'NOT NULL' else 'NULL' end nullable  
                                         FROM INFORMATION_SCHEMA.COLUMNS
                                         WHERE  TABLE_NAME = '%s';`, tableName))
 }
 
+// fetches n rows from the specified table
 func GetTableRows(dbConn DBConn, tableName string) (*Data, error) {
 	return ExecuteQuery(dbConn, fmt.Sprintf("SELECT * FROM %s limit %d;", tableName, getTableDataRowLimit()))
 }
 
+// executes a user supplied sql query or statement
 func ExecuteQuery(dbConn DBConn, query string) (*Data, error) {
-	var data Data
-
 	timeoutSecs := getTimeoutSecs()
 	queryCtx, cancel := context.WithTimeout(context.Background(), timeoutSecs*time.Second)
 	defer cancel()
 
-	// Execute the query
-	rows, err := dbConn.DB.QueryContext(queryCtx, query)
-	if err != nil {
-
-		return nil, err
-	}
-	defer rows.Close()
-
-	// Get column names
-	columns, err := rows.Columns()
+	stmt, err := sqlparser.Parse(query)
 	if err != nil {
 		return nil, err
 	}
-	data.Columns = columns
 
-	// Make a slice for the values
-	values := make([]sql.RawBytes, len(columns))
-
-	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
-	// references into such a slice
-	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
+	switch stmt.(type) {
+	case *sqlparser.Select:
+		return fetchRows(queryCtx, dbConn.DB, query)
+	default:
+		return execStatement(queryCtx, dbConn.DB, query)
 	}
-
-	// Fetch rows
-	for rows.Next() {
-		// get RawBytes from data
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-
-			return nil, err
-		}
-
-		// Now do something with the data.
-		// Here we just print each column as a string.
-		row := make(map[string]any, 0)
-		for i, val := range values {
-			// Here we can check if the value is nil (NULL value)
-			if val == nil {
-				row[columns[i]] = "NULL"
-			} else {
-				row[columns[i]] = string(val)
-			}
-		}
-		data.Rows = append(data.Rows, row)
-	}
-	if err = rows.Err(); err != nil {
-		if errors.Is(err, context.DeadlineExceeded) {
-			return nil, fmt.Errorf("query timeout exceeded (%d secs)\n\n to change the timeout add or modify the 'queryTimeout` config option", timeoutSecs)
-		}
-		return nil, err
-	}
-
-	return &data, nil
 }
 
 func getTimeoutSecs() time.Duration {
@@ -124,4 +80,77 @@ func getTableDataRowLimit() int {
 		return 100
 	}
 	return rowLimit
+}
+
+func fetchRows(ctx context.Context, db *sql.DB, query string) (*Data, error) {
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	data := &Data{Columns: columns, Rows: []map[string]interface{}{}}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, err
+		}
+
+		row := make(map[string]interface{})
+		for i, val := range values {
+			if val == nil {
+				row[columns[i]] = "NULL"
+			} else {
+				row[columns[i]] = string(val)
+			}
+		}
+		data.Rows = append(data.Rows, row)
+	}
+
+	if err = rows.Err(); err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, fmt.Errorf("query timeout exceeded (%d secs)\n\n to change the timeout add or modify the 'queryTimeout` config option", getTimeoutSecs())
+		}
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func execStatement(ctx context.Context, db *sql.DB, query string) (*Data, error) {
+	res, err := db.ExecContext(ctx, query)
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, fmt.Errorf("query timeout exceeded (%d secs)\n\n to change the timeout add or modify the 'queryTimeout` config option", getTimeoutSecs())
+		}
+		return nil, err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+	lastInsertId, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Data{
+		Columns: []string{"Rows Affected", "Last Inserted ID"},
+		Rows: []map[string]interface{}{{
+			"Rows Affected":    rowsAffected,
+			"Last Inserted ID": lastInsertId,
+		}}}, nil
+
 }
