8f894b681322593922bab7ccaa33d1bb3a2b956b
wheelibin <wheelibin@gmail.com>
2026-09-07 15:30:52 +0100

    chore: release v1.9.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "1.8.6";
+        version = "1.9.0";
       in
       {
         packages.default = pkgs.buildGoModule {
